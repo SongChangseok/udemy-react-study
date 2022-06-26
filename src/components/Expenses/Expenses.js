c665484e2b1598a030108320af1ee7e6fb1b1ba1
cssong <a1373052011@gmail.com>
2022-06-26 17:45:38 +0900

    filter 추가

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,12 +1,23 @@
+import { useState } from "react";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 
 export default function Expenses({ items }) {
+  const [filteredYear, setFilteredYear] = useState("2022");
+  const filterYearHandler = ({ target: { value } }) => {
+    setFilteredYear(value);
+    console.log(value);
+  };
   return (
     <Card className="expenses">
-      {items.map((value) => (
-        <ExpenseItem key={value.id} {...value} />
+      <ExpensesFilter
+        selectedYear={filteredYear}
+        onFilterYear={filterYearHandler}
+      />
+      {items.map((expense) => (
+        <ExpenseItem key={expense.id} {...expense} />
       ))}
     </Card>
   );
